71085c183c5e0f9f49a0afb780fdccc8e82e3e4d
evyatar-elad <evyatar613@gmail.com>
2025-12-31 01:48:37 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// This constant WILL change the bundle hash
+const BUILD_ID = "v2025-12-30-final";
+console.log("Build:", BUILD_ID);
+
 createRoot(document.getElementById("root")!).render(<App />);
-// Cache bust: 1735604400
